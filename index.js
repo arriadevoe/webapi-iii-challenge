030074e67c236c855c7c2e4e7d3d75a74bbd3570
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-21 12:04:23 -0400

    Installed and imported dotenv npm module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const server = require('./server.js');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log(`Server Running on http://localhost:${port}`)
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
 })
