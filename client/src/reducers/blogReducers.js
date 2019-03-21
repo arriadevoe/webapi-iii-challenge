47c361ddc2e888b5b6640f3266509754ecab848e
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-21 14:13:29 -0400

    Constructed blog reducer for GET posts with user ID

--- client/src/reducers/blogReducers.js
+++ client/src/reducers/blogReducers.js
@@ -5,53 +5,66 @@ import {
   SUCCESS_GET_ALL_POSTS,
   SUCCESS_GET_ALL_USERS,
   SUCCESS_GET_POSTS_BY_USER,
-  FAILURE,
-} from '../actions/actions';
+  FAILURE
+} from "../actions/actions";
 
 const initialState = {
   posts: [],
   userList: [],
   gettingPosts: false,
   gettingUsers: false,
-  error: null,
-}
+  error: null
+};
 
 export default (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case GET_ALL_POSTS:
       return {
         ...state,
         gettingPosts: true,
-        error: null,
-      }
-      case GET_ALL_USERS:
+        error: null
+      };
+    case GET_ALL_USERS:
+      return {
+        ...state,
+        gettingUsers: true,
+        error: null
+      };
+    case GET_POSTS_BY_USER:
       return {
         ...state,
         gettingUsers: true,
         error: null,
-      }
+      };
     case SUCCESS_GET_ALL_POSTS:
       return {
         ...state,
         posts: action.payload,
         gettingPosts: false,
-        error: null,
-      }
+        error: null
+      };
     case SUCCESS_GET_ALL_USERS:
       return {
         ...state,
         userList: action.payload,
         gettingUsers: false,
-        error: null,
-      }
+        error: null
+      };
+    case SUCCESS_GET_POSTS_BY_USER:
+      return {
+        ...state,
+        posts: action.payload,
+        gettingUsers: false,
+        error: null
+      };
     case FAILURE:
       return {
         ...state,
         gettingPosts: false,
         gettingUsers: false,
-        error: action.payload,
-      }
+        error: action.payload
+      };
     default:
-      return state
+      return state;
   }
-}
+};
