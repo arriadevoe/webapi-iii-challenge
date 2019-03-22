4c314fb5e0fb0cab6ea0ed5f1a5de684ed1277e6
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-22 00:22:13 -0400

    Removed commented artifacts

--- client/src/components/CreateNewPost/index.js
+++ client/src/components/CreateNewPost/index.js
@@ -3,38 +3,18 @@ import { connect } from "react-redux";
 
 import { createNewPost, getPostsByUser } from "../../actions/actions";
 
-import { DropdownMenu, DropdownToggle, Dropdown, Form, Label, Input, Button } from "reactstrap";
+import {
+  DropdownMenu,
+  DropdownToggle,
+  Dropdown,
+  Form,
+  Label,
+  Input,
+  Button
+} from "reactstrap";
 import "./CreateNewPost.css";
 
 const CreateNewPost = props => {
-  // const filterByAuthor = e => {
-  //   e.preventDefault();
-
-  //   if (currentInputState) {
-  //     let upperCaseArray = [];
-
-  //     for (let name of currentInputState.split(" ")) {
-  //       upperCaseArray.push(name.charAt(0).toUpperCase() + name.slice(1));
-  //     }
-
-  //     let foundCharacterArray = props.userList.filter(user => user.name === upperCaseArray.join(' '));
-
-  //     if (foundCharacterArray.length) {
-  //       props.getPostsByUser(foundCharacterArray[0].id);
-  //     } else {
-  //       alert ("No character with this name exists.")
-  //     } setInputState("");
-
-  //   } else {
-  //     alert("Please enter a character name.");
-  //   }
-  // };
-
-  // const viewAll = e => {
-  //   e.preventDefault();
-
-  //   props.getAllPosts();
-  // };
   const [dropdownOpen, dropdownToggle] = useState(false);
   const [currentText, setInputText] = useState("");
   const [currentName, setInputName] = useState("");
@@ -58,45 +38,38 @@ const CreateNewPost = props => {
           upperCaseArray.push(name.charAt(0).toUpperCase() + name.slice(1));
         }
 
-        let foundCharacterArray = props.userList.filter(user => user.name === upperCaseArray.join(' '));
+        let foundCharacterArray = props.userList.filter(
+          user => user.name === upperCaseArray.join(" ")
+        );
 
         if (foundCharacterArray.length) {
-          props.createNewPost({"user_id": foundCharacterArray[0].id , "text": currentText});
+          props.createNewPost({
+            user_id: foundCharacterArray[0].id,
+            text: currentText
+          });
           props.getPostsByUser(foundCharacterArray[0].id);
-                  
+
           setInputName("");
           setInputText("");
         } else {
-          alert("This character name does not exist.")
-        } 
+          alert("This character name does not exist.");
+        }
       } else {
         alert("Please do not leave any blank fields.");
       }
-
-      // const stateCopy = {...this.state};
-      
-      // this.props.updateSmurf(id, stateCopy)
-      // props.createNewPost
-  
-      // this.setState(prevState => ({
-      //   dropdownOpen: !prevState.dropdownOpen,
-      //   name: '',
-      //   age: '',
-      //   height: '',
-      // }));
     };
-  }
+  };
 
   return (
-    <Dropdown isOpen={dropdownOpen} toggle={() => dropdownToggle(!dropdownOpen)}>
+    <Dropdown
+      isOpen={dropdownOpen}
+      toggle={() => dropdownToggle(!dropdownOpen)}
+    >
       <DropdownToggle color="success" className="quote-dropdown" caret>
         NEW QUOTE
       </DropdownToggle>
-      <DropdownMenu className='quote-menu' >
-        <Form
-          className="quote-form"
-          onSubmit={handleSubmit()}
-        >
+      <DropdownMenu className="quote-menu">
+        <Form className="quote-form" onSubmit={handleSubmit()}>
           <Label className="quote-label">
             Character Name:
             <Input
